969a7e8ce4b473b979e46e8080ce9cc5c7f638c3
MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>
2021-05-05 14:53:27 -0500

    Member gui

---
+++ NetBeansProjects/SistemaCuerpoAcademico/src/GUI/Launcher.java
@@ -0,0 +1,33 @@
+
+package GUI;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+  
+        try {
+            URL url = new File("src/GUI/MemberView.fxml").toURI().toURL();
+            AnchorPane root = (AnchorPane)FXMLLoader.load(url);
+            Scene scene = new Scene(root,600,400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(Launcher.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+       
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}
--- NetBeansProjects/SistemaCuerpoAcademico/src/GUI/MemberViewController.java
+++ NetBeansProjects/SistemaCuerpoAcademico/src/GUI/MemberViewController.java
@@ -1,27 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUI;
 
+
+import businessLogic.MemberDAO;
+import domain.Member;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import log.BusinessException;
 
-/**
- * FXML Controller class
- *
- * @author Mariana
- */
+
 public class MemberViewController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    private ObservableList<String> roles;
+    private ObservableList<String> degrees;
+    private ObservableList<Integer> years;
+    @FXML private ComboBox<Integer> cbYears;
+    @FXML private ComboBox<String> cbRoles;
+    @FXML private ComboBox<String> cbDegrees;
+    @FXML private TextField tfName;
+    @FXML private TextField tfProfessionalLicense;
+    @FXML private TextField tfNameDegree;
+    @FXML private TextField tfUniversity;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+     roles = FXCollections.observableArrayList();
+     roles.add("Integrante");
+     roles.add("Colaborador");
+     roles.add("Responsable");
+     cbRoles.setItems(roles);
+     degrees = FXCollections.observableArrayList();
+     degrees.add("Maestria");
+     degrees.add("Doctorado");
+     cbDegrees.setItems(degrees);
+     years = FXCollections.observableArrayList();
+     for(int i = getActualYear(); i>1899; i--){
+            years.add(i);
+     }
+     cbYears.setItems(years);
+     cbYears.getSelectionModel().selectFirst();
     }    
+
+    @FXML 
+    public void save(){
+       String name = "", role = "", degree = "", professionalLicense = "", nameDegree = "", universityName = "";
+       int degreeYear = 0;
+       name = tfName.getText();
+       role = cbRoles.getSelectionModel().getSelectedItem();
+       degree = cbDegrees.getValue();
+       professionalLicense = tfProfessionalLicense.getText();
+       nameDegree = tfNameDegree.getText();
+       degreeYear = cbYears.getSelectionModel().getSelectedItem();
+       universityName = tfUniversity.getText();
+       Member newMember = new Member(professionalLicense, name, role, degree,nameDegree,universityName, degreeYear,"1491");
+       MemberDAO memberDAO = new MemberDAO();
+        try { 
+            memberDAO.saveMember(newMember);
+        } catch (BusinessException ex) {
+            ex.printStackTrace();
+        }
+    }
     
+    public int getActualYear(){
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.YEAR);
+    }
+    
+    public boolean isEmptyFields(Member member){
+        boolean emptyFields = FALSE;
+        if((member.getProfessionalLicense().isEmpty())||(member.getName().isEmpty())||(member.getRole().isEmpty())||(member.getDegree().isEmpty())|| (member.getNameDegree().isEmpty())||(member.getUniversityName().isEmpty())|| member.getDegreeYear() == 0){
+            emptyFields = TRUE;
+        }
+        return emptyFields;
+    }
 }
