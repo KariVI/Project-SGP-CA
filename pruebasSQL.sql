d8c583c752acd40a544edcdcfbc2a36896673fbf
KariVI <47987287+KariVI@users.noreply.github.com>
2021-06-12 00:20:52 -0500

    Update pruebas

--- pruebasSQL.sql
+++ pruebasSQL.sql
@@ -50,6 +50,13 @@ INSERT INTO Dirige(idTrabajoRecepcional, cedula,rol) VALUES (3,"8325134","Direct
 INSERT INTO ParticipaTrabajoRecepcional(idTrabajoRecepcional,matricula) values (3,"S19014023");
 INSERT INTO ParticipaTrabajoRecepcional(idTrabajoRecepcional,matricula) values (4,"S19014013");
 INSERT INTO CultivaTrabajoRecepcional(idTrabajoRecepcional, nombreLGAC) values(3,"Tecnologías de software");
+INSERT INTO Credenciales(cedula,usuario,contrasenia) VALUES("8325134","JuanPer",hex(AES_ENCRYPT("12345",'key')));
 
+INSERT INTO Reunion(asunto,hora, fecha, estado,clave_CA) VALUES ("Revisión de avances en proyectos actuales" ,"04/08/2021","11:30","Registrada", "JDOEIJ804");
+INSERT INTO Reunion(asunto,hora, fecha, estado,clave_CA) VALUES( "Organizar agenda de actividades del seminario" ,"15/09/2021","15:30","Concluida","JDOEIJ804");
+INSERT INTO Reunion(asunto,hora, fecha, estado,clave_CA) VALUES("Actualizar plan de trabajo de la LIS","13:00","11/08/2021", "Concluida","JDOEIJ804");
+INSERT INTO Reunion(asunto,hora, fecha, estado,clave_CA) VALUES("Lanzamiento FEIBook" ,"11/11/2021","12:30","Registrada","JDOEIJ804");
 
+INSERT INTO ParticipaReunion(idReunion, cedula, rol) VALUES (2,"8325134","Secretario");
+INSERT INTO ParticipaReunion(idReunion,cedula,rol) VALUES(4,"7938268","Lider");
 SELECT * FROM TrabajoRecepcional where clave_CA="JDOEIJ804";
