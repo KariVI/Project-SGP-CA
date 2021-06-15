008633f469b08eb35d124b74e17fac00d91f3ce7
MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>
2021-06-15 18:54:05 -0500

    member method get
    Some methods that already use this, i live them static

--- NetBeansProjects/SistemaCuerpoAcademico/src/GUI/ReceptionWorkRegisterController.java
+++ NetBeansProjects/SistemaCuerpoAcademico/src/GUI/ReceptionWorkRegisterController.java
@@ -558,7 +558,7 @@ public class ReceptionWorkRegisterController implements Initializable {
         try {
             MemberDAO memberDAO = new MemberDAO();
             ArrayList <Member> memberList = new ArrayList<Member>();
-            memberList = memberDAO.getMembers("JDOEIJ804");
+            memberList = memberDAO.getMembers(keyGroupAcademic);
             for( int i = 0; i<memberList.size(); i++) {
                 members.add(memberList.get(i));
             }
