bc9ef21de37fc5fe214a6e697ea45f1a75519d32
KariVI <47987287+KariVI@users.noreply.github.com>
2022-02-27 21:15:06 -0600

    Actualización CU-21

--- NetBeansProjects/SistemaCuerpoAcademico/src/domain/Goal.java
+++ NetBeansProjects/SistemaCuerpoAcademico/src/domain/Goal.java
@@ -54,4 +54,9 @@ public class Goal {
         }
         return value;
     }
+       
+    @Override
+    public String toString(){
+        return description;
+    }
 }
--- NetBeansProjects/SistemaCuerpoAcademico/src/domain/WorkPlan.java
+++ NetBeansProjects/SistemaCuerpoAcademico/src/domain/WorkPlan.java
@@ -20,7 +20,6 @@ public class WorkPlan {
     }
 
     public WorkPlan() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getId() {
