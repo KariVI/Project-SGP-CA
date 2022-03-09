b0694324a507339744443016f3b5df8115b3399d
MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>
2022-03-09 00:13:32 -0600

    Update project gui again

--- NetBeansProjects/SistemaCuerpoAcademico/test/businessLogic/ActionDAOTest.java
+++ NetBeansProjects/SistemaCuerpoAcademico/test/businessLogic/ActionDAOTest.java
@@ -17,40 +17,41 @@ import org.junit.Test;
  * @author david
  */
 public class ActionDAOTest {
+
     @Test
     public void getActionsByGoalId() throws BusinessException {
         System.out.println("getActions");
         int idGoal = 1;
-        Action action = new Action(2,"Obtener una lista de contactos de los CA","Diciembre 2020","KVC","Lista de cuerpos académicos de PRODEP");
+        Action action = new Action(2, "Obtener una lista de contactos de los CA", "Diciembre 2020", "KVC", "Lista de cuerpos académicos de PRODEP");
         ArrayList<Action> resultExpected = new ArrayList<Action>();
         resultExpected.add(action);
         ActionDAO actionDAO = new ActionDAO();
-        ArrayList<Action> result = actionDAO.getActionsByGoalId(idGoal);   
-        assertEquals(result,resultExpected);
+        ArrayList<Action> result = actionDAO.getActionsByGoalId(idGoal);
+        assertEquals(result, resultExpected);
     }
-    
+
     @Test
     public void testDeleteAction() throws BusinessException {
         System.out.println("delete");
         ActionDAO actionDAO = new ActionDAO();
         assertTrue(actionDAO.deletedActionById(5));
     }
-    
+
     @Test
     public void testUpdateAction() throws BusinessException {
         System.out.println("update");
-        Action newAction = new Action(2, "Obtener una lista de contactos de los CAs","Diciembre 2020","KVC","Lista de cuerpos académicos de PRODEP");
+        Action newAction = new Action(2, "Obtener una lista de contactos de los CAs", "Diciembre 2020", "KVC", "Lista de cuerpos académicos de PRODEP");
         ActionDAO actionDAO = new ActionDAO();
         int idGoal = 1;
         assertTrue(actionDAO.updatedActionById(idGoal, newAction));
     }
-    
+
     @Test
-    public void testSave() throws BusinessException{
+    public void testSave() throws BusinessException {
         System.out.println("saveSuccesful");
-        Action action = new Action(1,"Obtener una lista de los anteproyectos","Diciembre 2022","JOOH","Lista de cuerpos académicos de PRODEP, Anteproyectos disonibles");
+        Action action = new Action(1, "Obtener una lista de los anteproyectos", "Diciembre 2022", "JOOH", "Lista de cuerpos académicos de PRODEP, Anteproyectos disonibles");
         ActionDAO actionDAO = new ActionDAO();
-        int idGoal=1;
+        int idGoal = 1;
         assertTrue(actionDAO.saveSuccesful(action, idGoal));
     }
 }
