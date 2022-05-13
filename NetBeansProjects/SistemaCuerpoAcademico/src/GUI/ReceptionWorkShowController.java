b07fc95ddb60700945346c31ae1760fb918e8b97
DavidMijangos08 <78769160+DavidMijangos08@users.noreply.github.com>
2022-05-13 17:40:35 -0500

    Correción por conflicto

--- NetBeansProjects/SistemaCuerpoAcademico/src/GUI/ReceptionWorkShowController.java
+++ NetBeansProjects/SistemaCuerpoAcademico/src/GUI/ReceptionWorkShowController.java
@@ -144,13 +144,7 @@ public class ReceptionWorkShowController implements Initializable {
        lbEndDate.setText("Fecha fin: "+receptionWork.getDateEnd());
        lbType.setText("Tipo:   " +receptionWork.getType());
        taDescription.setText("Descripción: " + receptionWork.getDescription());
-
-<<<<<<< HEAD
-       //lbPreliminarProject. setText("Proyecto: "+ receptionWork.getProject());
-=======
        lbProject. setText("Proyecto: "+ receptionWork.getProject());
->>>>>>> 4e8a2bb1671b8ac490ffe9a6d086902de116e29e
-
        lbState.setText("Estado: "+ receptionWork.getActualState());
        recoverColaborators ();
            try {
